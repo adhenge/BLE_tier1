228cc544761ab9d2834d79a9886267a9fbf8630a
adhenge <adhenge@uncc.edu>
2016-12-16 16:27:59 -0500

    ble_tier1 node
    initial code for tier 1

---
+++ pca10040/s132/arm4/_build/nrf_nvic.d
@@ -0,0 +1,17 @@
+.\_build\nrf_nvic.o: ..\..\..\..\..\..\components\drivers_nrf\nrf_soc_nosd\nrf_nvic.c
+.\_build\nrf_nvic.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
+.\_build\nrf_nvic.o: ..\..\..\..\..\..\components\drivers_nrf\nrf_soc_nosd\nrf_soc.h
+.\_build\nrf_nvic.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\_build\nrf_nvic.o: ..\..\..\..\..\..\components\device\nrf.h
+.\_build\nrf_nvic.o: ..\..\..\..\..\..\components\device\nrf52.h
+.\_build\nrf_nvic.o: ..\..\..\..\..\..\components\toolchain\CMSIS\Include\core_cm4.h
+.\_build\nrf_nvic.o: ..\..\..\..\..\..\components\toolchain\CMSIS\Include\core_cmInstr.h
+.\_build\nrf_nvic.o: ..\..\..\..\..\..\components\toolchain\CMSIS\Include\cmsis_armcc.h
+.\_build\nrf_nvic.o: ..\..\..\..\..\..\components\toolchain\CMSIS\Include\core_cmFunc.h
+.\_build\nrf_nvic.o: ..\..\..\..\..\..\components\toolchain\CMSIS\Include\core_cmSimd.h
+.\_build\nrf_nvic.o: ..\..\..\..\..\..\components\toolchain\system_nrf52.h
+.\_build\nrf_nvic.o: ..\..\..\..\..\..\components\device\nrf52_bitfields.h
+.\_build\nrf_nvic.o: ..\..\..\..\..\..\components\device\nrf51_to_nrf52.h
+.\_build\nrf_nvic.o: ..\..\..\..\..\..\components\device\nrf52_name_change.h
+.\_build\nrf_nvic.o: ..\..\..\..\..\..\components\device\compiler_abstraction.h
+.\_build\nrf_nvic.o: ..\..\..\..\..\..\components\drivers_nrf\nrf_soc_nosd\nrf_error.h
